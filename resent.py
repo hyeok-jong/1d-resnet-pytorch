ac61c214491b36f3ffef62db75ddcddf10c56738
HyeokJong <offic.hyeokjong@gmail.com>
2023-08-16 14:53:20 +0900

    update

--- resent.py
+++ resent.py
@@ -13,30 +13,20 @@ class Residual_Block(nn.Module):
         assert kernel_size % 2 ==1, f'kernel_size {kernel_size} should be odd'
         
         padding = (kernel_size - 1) // 2
-        
-        self.skip_connection = nn.Conv1d(
-            in_channels = in_channels,
-            out_channels = out_channels,
-            kernel_size = kernel_size,
-            stride = 1,
-            padding = padding
-        )
-        
+
+            
         Conv_Blocks = list()
         for n in range(n_layers):
             if n != 0:
                 in_channels = out_channels
             Conv_Blocks.append(
-                self.BaseModule(in_channels, out_channels, kernel_size, padding, drop_p, last = n == n_layers - 1)
+                self.BaseModule(in_channels, out_channels, kernel_size, padding, drop_p)
             )
         self.Conv_Blocks = nn.Sequential(*Conv_Blocks)
 
-
-
         self.ReLU = nn.ReLU()
 
-
-    def BaseModule(self, in_channels, out_channels, kernel_size, padding, drop_p, last):
+    def BaseModule(self, in_channels, out_channels, kernel_size, padding, drop_p):
         Conv_Block = [
             nn.Conv1d(
                 in_channels = in_channels,
@@ -51,27 +41,25 @@ class Residual_Block(nn.Module):
             nn.ReLU(),
             nn.Dropout(p = drop_p)
         ]
-        if last:
-            Conv_Block.pop(-2)
         return nn.Sequential(*Conv_Block)
 
     def forward(self, x):
         fx = self.Conv_Blocks(x)
-        return self.ReLU(fx + self.skip_connection(x))
+        return self.ReLU(fx + x)
 
 class Reduction(nn.Module):
-    def __init__(self, channels, drop_p):
+    def __init__(self, in_channels, out_channels, kernel_size, drop_p):
         super(Reduction, self).__init__()
         self.reduction_layer = nn.Sequential(
             nn.Conv1d(
-                in_channels = channels,
-                out_channels = channels,
-                kernel_size = 3,
-                stride = 2,
+                in_channels = in_channels,
+                out_channels = out_channels,
+                kernel_size = kernel_size,
+                stride = 1,
                 padding = 0
             ),
             nn.BatchNorm1d(
-                num_features = channels
+                num_features = out_channels
             ),
             nn.ReLU(),
             nn.Dropout(p = drop_p) )
@@ -82,26 +70,9 @@ class Reduction(nn.Module):
 
 class MyResNet(nn.Module):
     def __init__(self, in_channels, out_channels, n_classes, kernel_size, block_list, n_layers, drop_p):
-        '''
-        in_channels : input shape (batch, in_channels, n_length)
-        n_classes : output shape e.g) binary classification can 1 or 2
-        block_list : list of blocks
-        n_layers : number of layers in one block
-        drop_p : proportion in dropout
 
-        Features
-        1. Batch Normalization included,
-        2. All the 1D-CNN have same parameters,
-        3. input shape should follow (batch, channel, length of sequence).
-        4. No activation function on last layer, so when compute loss, one should use nn.CrossEntropyLoss or nn.BCEWithLogitsLoss
-           which including activation function.
-        5. 
-        '''
         super(MyResNet, self).__init__()
-        self.in_channels = in_channels
-        self.out_channels = out_channels
-        self.kernel_size = kernel_size
-        self.drop_p = drop_p
+
 
         self.encoder, final_channel = self.make_encoder(in_channels, out_channels, kernel_size, block_list, n_layers, drop_p)
 
@@ -117,16 +88,17 @@ class MyResNet(nn.Module):
         encoder = nn.Sequential()
         
         for idx, blocks in enumerate(block_list):
-            
+            encoder.add_module(f'Reduction{idx+1}', Reduction(in_channels, out_channels, kernel_size, drop_p))
             for n in range(blocks):
-                encoder.add_module(f'Resblock{idx+1}_{n+1}', Residual_Block(in_channels, out_channels, kernel_size, n_layers, drop_p))
-                in_channels = out_channels
-            
-                
-            encoder.add_module(f'Reduction{idx+1}', Reduction(out_channels,drop_p))
+                encoder.add_module(f'Resblock{idx+1}_{n+1}', Residual_Block(out_channels, out_channels, kernel_size, n_layers, drop_p))
+
+            in_channels = out_channels
             out_channels *= 2
             
-        return encoder, out_channels//2
+                
+            
+            
+        return encoder, in_channels
 
     def forward(self, x):
         x = self.encoder(x)
